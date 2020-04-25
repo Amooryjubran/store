b92ec9702374df639e6374fc05eeb09f8de0522e
amooryjubran <omarsalehjubran@gmail.com>
2020-04-25 00:10:20 -0400

    page structures

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FaHome} from 'react-icons/fa';
+import HomePage from './components/pages/HomePage';
+import AboutPage from './components/pages/AboutPage';
+import ProductsPage from './components/pages/ProductsPage';
+import ContactPage from './components/pages/ContactPage';
+import CartPage from './components/pages/CartPage';
+import Default from './components/pages/Default';
+import SingleProductPage from './components/pages/SingleProductPage';
+
 
 function App() {
   return (
-    
-      <h1>omar <FaHome/></h1>
+    <>
+    <HomePage/>
+    <AboutPage />
+    <ProductsPage/>
+    <ContactPage />
+    <CartPage />
+    <Default/>
+    <SingleProductPage />
+    <h1>hello from store </h1>
+    </>
     
   );
 }
+
+
 
 export default App;
