db799a71503613b25aa7490077873ece52f4b8f4
amooryjubran <omarsalehjubran@gmail.com>
2020-04-27 00:09:26 -0400

    services

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Hero from '../components/Hero';
+import Features from '../components/HomePage/Features';
+import Services from '../components/HomePage/Services';
 
 
 export default function HomePage() {
     return (
         <>
+        
             <Hero title="awesome gadgets">
                 <Link to ="/products" style={{margin: '2rem'}} className="main-link">
                     out products
+                    
                 </Link>
             </Hero>
+            <Services />
+            <Features />
         </>
     )
 }
