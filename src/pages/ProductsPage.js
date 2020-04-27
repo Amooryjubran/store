8e468081560911c29dfc66756992e0db4f0b6479
amooryjubran <omarsalehjubran@gmail.com>
2020-04-27 15:25:59 -0400

    all products page

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Products from '../components/ProductsPage/Products';
+import Hero from '../components/Hero';
+import productsBcg from '../images/productsBcg.jpeg'
 
 export default function ProductsPage() {
     return (
         <>
-            <h1>wewewe</h1>
+            <Hero img={productsBcg}/>
+            <Products />
+            
         </>
     )
 }
